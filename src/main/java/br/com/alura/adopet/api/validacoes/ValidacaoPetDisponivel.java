bc3fe0995a64525e69fdc93b477210170bb938fc
murilomts <murilo.mtds@gmail.com>
2024-01-21 16:56:54 -0300

    Implementando padrão strategy

---
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivel.java
@@ -0,0 +1,24 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidacaoPetDisponivel implements ValidacaoSolicitacaoAdocao {
+
+    @Autowired
+    private PetRepository petRepository;
+
+    @Override
+    public void validar(SolicitacaoAdocaoDTO dto) {
+        Pet pet = petRepository.getReferenceById(dto.idPet());
+        if (pet.getAdotado()) {
+            throw new ValidacaoException("Pet já foi adotado!");
+        }
+    }
+
+}
